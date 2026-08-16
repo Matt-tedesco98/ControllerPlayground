4b453266b405fb63259e367885aa8ebd86d523e8
Matt-tedesco98 <matthewtedesco3@gmail.com>
2026-08-15 23:54:01 -0400

    feat: make controller prompt bar configurable

--- Controls/ControllerPromptBar.xaml.cs
+++ Controls/ControllerPromptBar.xaml.cs
@@ -20,8 +20,8 @@ namespace ControllerPlayground.Controls {
         }
 
         private static void OnFamilyChanged(
-            DependencyObject d, 
-            DependencyPropertyChangedEventArgs e ) {
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e) {
             if (d is ControllerPromptBar bar) {
                 bar.UpdatePrompt();
             }
@@ -34,5 +34,66 @@ namespace ControllerPlayground.Controls {
             ViewPrompt.Family = Family;
             MenuPrompt.Family = Family;
         }
+
+        public static readonly DependencyProperty ShowAcceptProperty = DependencyProperty.Register(
+            nameof(ShowAccept),
+            typeof(bool),
+            typeof(ControllerPromptBar),
+            new PropertyMetadata(true, OnPromptVisibilityChanged));
+
+
+        public static readonly DependencyProperty ShowBackProperty = DependencyProperty.Register(
+            nameof(ShowBack),
+            typeof(bool),
+            typeof(ControllerPromptBar),
+            new PropertyMetadata(true, OnPromptVisibilityChanged));
+
+        public static readonly DependencyProperty ShowViewProperty = DependencyProperty.Register(
+            nameof(ShowView),
+            typeof(bool),
+            typeof(ControllerPromptBar),
+            new PropertyMetadata(true, OnPromptVisibilityChanged));
+
+        public static readonly DependencyProperty ShowMenuProperty = DependencyProperty.Register(
+            nameof(ShowMenu),
+            typeof(bool),
+            typeof(ControllerPromptBar),
+            new PropertyMetadata(true, OnPromptVisibilityChanged));
+
+        public bool ShowAccept {
+            get => (bool)GetValue(ShowAcceptProperty);
+            set => SetValue(ShowAcceptProperty, value);
+        }
+
+        public bool ShowBack {
+            get => (bool)GetValue(ShowBackProperty);
+            set => SetValue(ShowBackProperty, value);
+        }
+
+        public bool ShowView {
+            get => (bool)GetValue(ShowViewProperty);
+            set => SetValue(ShowViewProperty, value);
+        }
+
+        public bool ShowMenu {
+            get => (bool)GetValue(ShowMenuProperty);
+            set => SetValue(ShowMenuProperty, value);
+        }
+
+        private static void OnPromptVisibilityChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e) {
+            if (d is ControllerPromptBar bar) {
+                bar.UpdatePromptVisibility();
+            }
+        }
+
+        private void UpdatePromptVisibility() {
+            AcceptPrompt.Visibility = ShowAccept ? Visibility.Visible : Visibility.Collapsed;
+            BackPrompt.Visibility = ShowBack ? Visibility.Visible : Visibility.Collapsed;
+            ViewPrompt.Visibility = ShowView ? Visibility.Visible : Visibility.Collapsed;
+            MenuPrompt.Visibility = ShowMenu ? Visibility.Visible : Visibility.Collapsed;
+        }
     }
 }
+    
